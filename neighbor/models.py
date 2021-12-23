228921f3753e09f6e27137646212ba4e74011469
mwendaB <brian.mwenda@student.moringaschool.com>
2021-12-23 13:58:30 +0300

    create the profile class

--- neighbor/models.py
+++ neighbor/models.py
@@ -34,3 +34,35 @@ class Neighbourhood(models.Model):
     @property
     def occupants_count(self):
         return self.neighbourhood_users.count() 
+
+
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    idNo = models.IntegerField(default=0)
+    email = models.CharField(max_length=30, blank=True)
+    profile_pic = CloudinaryField('profile')
+    bio = models.TextField(max_length=500, blank=True)
+    neighbourhood = models.ForeignKey(
+        Neighbourhood, on_delete=models.CASCADE, blank=True, null=True)
+
+    def __str__(self):
+        return self.user.username
+
+    @receiver(post_save, sender=User)
+    def update_user_profile(sender, instance, created, **kwargs):
+        if created:
+            Profile.objects.create(user=instance)
+        instance.profile.save()
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile(self):
+        self.delete()
+
+    def update_profile(cls, id):
+        Profile.objects.get(user_id=id)
+
+
