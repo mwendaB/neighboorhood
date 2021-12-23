ab49b314403aa232ffe9da1cef79f67c5c56d7a6
mwendaB <brian.mwenda@student.moringaschool.com>
2021-12-23 14:26:29 +0300

    add the signup section

--- neighbor/views.py
+++ neighbor/views.py
@@ -6,10 +6,27 @@ from django.http.response import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required
-from django.contrib.auth import login, authentica
+from django.contrib.auth import login, authenticate
 
 
 def index(request):
   
     neigborhoods = Neighborhood.objects.all()
     return render(request, 'index.html', {"neigborhoods": neigborhoods})
+
+
+def signup(request):
+    print('here')
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            user.save()
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=user.username, password=raw_password)
+
+            login(request, user)
+            return redirect('login')
+    else:
+        form = SignUpForm()
+    return render(request, 'registration/registration_form.html', {'form': form})
