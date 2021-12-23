ce4ab8a046b4da7b70665be2007b1e14ba76e63f
mwendaB <brian.mwenda@student.moringaschool.com>
2021-12-23 14:17:00 +0300

    add the post section form

--- neighbor/forms.py
+++ neighbor/forms.py
@@ -39,3 +39,8 @@ class NewBusinessForm(forms.ModelForm):
     class Meta:
         model = Business
         exclude = ('user',)
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        exclude = ('user',)
