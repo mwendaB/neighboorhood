17a1a8f9fe31e48ae5cf963b69477e83103a25c6
mwendaB <brian.mwenda@student.moringaschool.com>
2021-12-23 14:10:10 +0300

    ad imports to the form files

---
+++ neighbor/forms.py
@@ -0,0 +1,4 @@
+from .models import Business, Neighbourhood, Post, Profile
+from django.contrib.auth.models import User
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
