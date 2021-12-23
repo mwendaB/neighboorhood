87575b820b0ae0acd5f4f15484282b9bfba0b55b
mwendaB <brian.mwenda@student.moringaschool.com>
2021-12-23 14:09:18 +0300

    add imports to the views.py files

--- neighbor/views.py
+++ neighbor/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
-
-# Create your views here.
+from .models import Business, Post, Profile, Neighbourhood
+from django.contrib.auth.models import User
+from .forms import NewBusinessForm, NewHoodForm, PostForm, SignUpForm, UpdateProfileForm, UpdateUserForm
+from django.http.response import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import login, authentica
