37a18b0ccfab1b2ea508d9363ccd7f4248b8cb41
mwendaB <brian.mwenda@student.moringaschool.com>
2021-12-23 14:05:18 +0300

    create the bussiness cLASS

--- neighbor/models.py
+++ neighbor/models.py
@@ -83,3 +83,38 @@ class Post(models.Model):
         self.delete()
 
 
+class Business(models.Model):
+    business_name = models.CharField(max_length=250)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    business_hood = models.ForeignKey(Neighbourhood, on_delete=models.CASCADE)
+    business_email = models.CharField(max_length=30)
+    business_desc = models.TextField(blank=True)
+
+
+    def __str__(self):
+       
+        return f'{self.business_name} business'
+
+    def save_business(self):
+        self.save()
+
+    def delete_business(self):
+        self.delete()
+
+    @classmethod
+    def get_business(cls, business_id):
+        business = cls.objects.get(id=business_id)
+        return business
+
+    @classmethod
+    def business_by_id(cls, id):
+        business = Business.objects.filter(id=id)
+        return business
+
+    def update_business(self):
+        name = self.business_name
+        self.business_name = name
+
+    @classmethod
+    def search_business(cls, name):
+        return cls.objects.filter(business_name__icontains=name).all()
