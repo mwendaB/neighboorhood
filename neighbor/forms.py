676e7106a5a534be1b615ad3cc942193289c1f7b
mwendaB <brian.mwenda@student.moringaschool.com>
2021-12-23 14:10:57 +0300

    add class sign in section in forms

--- neighbor/forms.py
+++ neighbor/forms.py
@@ -2,3 +2,11 @@ from .models import Business, Neighbourhood, Post, Profile
 from django.contrib.auth.models import User
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
+
+
+class SignUpForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2', )
