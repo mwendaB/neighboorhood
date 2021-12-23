03beb1402fdc133574a34105ee9fe80ca2400e26
mwendaB <brian.mwenda@student.moringaschool.com>
2021-12-23 14:53:46 +0300

    add the admin section

--- neighbor/admin.py
+++ neighbor/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from neighbor.models import Business, Neighborhood, Post, Profile
 
 # Register your models here.
+
+admin.site.register(Neighborhood)
+admin.site.register(Profile)
+admin.site.register(Business)
+admin.site.register(Post)
