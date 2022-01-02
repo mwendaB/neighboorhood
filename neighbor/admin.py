c0ac2ffbbd4239f4f83f605676e9d83bc4685dab
mwendaB <brian.mwenda@student.moringaschool.com>
2022-01-02 21:16:16 +0300

    write the admin site

--- neighbor/admin.py
+++ neighbor/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Neighbourhood, Profile, Business, Post
 
 # Register your models here.
+admin.site.register(Profile) 
+admin.site.register(Neighbourhood) 
+admin.site.register(Post) 
+admin.site.register(Business) 
